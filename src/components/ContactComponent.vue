a38b873e66f84c0260b2bc9bdc51577ab89318f8
DevPatrickSousa <patrickseven22@hotmail.com>
2023-01-31 03:20:19 -0300

    finishing contact view

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -1,26 +1,25 @@
 <template>
-    <v-main class="bg-grey-darken-4 fill-height">
+    <v-main class="bg-grey-darken-4 fill-height ">
         <div class="d-flex mt-2 justify-center text-center ">
             <span>Contact</span>
         </div>
 
-        <div class="d-flex justify-center text-center">
-            <v-card class="mt-16 rounded-xl " variant="outlined" width="1500" height="700">
+        <div class="d-flex justify-center text-center ">
+            <v-card class="mt-16 rounded-xl" color="black" variant="outlined" width="1500" height="700">
                 <div class="mt-10">
                     <img src="/src/assets/whatsapp.ico" alt="whatsApp" ALIGN=middle>
-                    <span class="mx-7 mt-5">55 (11)94364-6430</span>
+                    <span class="mx-7 mt-5 text-white">55 (11)94364-6430</span>
                     <img src="/src/assets/outlook.ico" alt="outlook" ALIGN=middle>
-                    <span class="mx-7">patrickseven22@hotmail.com</span>
+                    <span class="mx-7 text-white">patrickseven22@hotmail.com</span>
                 </div>
 
                 <div class="d-flex justify-center align-center text-center">
                     <v-card class="d-flex mt-16 rounded-xl  justify-center align-center  " width="900" height="510"
                         variant="outlined">
-                        <form action="https://formsubmit.co/patrickseven22@hotmail.com" method="POST">
-                            <v-responsive class="mx-auto" min-width="700">
-                                <formComponent/>
-                            </v-responsive>
-                        </form>
+
+                        <formComponent />
+
+
                     </v-card>
                 </div>
             </v-card>
--- src/components/formComponent.vue
+++ src/components/formComponent.vue
@@ -1,48 +1,73 @@
 <template>
-    <v-text-field 
-    class="my-3" 
-    hide-details="auto"
-    label="Name"
-    type="text"
-    name="name"
-    required>
-    </v-text-field>
+    <v-form fast-fail ref="form" action="https://formsubmit.co/patrickseven22@hotmail.com" method="POST">
+        <v-responsive class="mx-auto" min-width="700">
+            <v-text-field :rules="textRules" class="my-3 text-white" hide-details="auto" label="Name" type="text" name="name" clearable required>
+            </v-text-field>
 
-    <v-text-field 
-    class="my-3" 
-    hide-details="auto"
-    label="Email"
-    type="email"
-    name="email"
-    placeholder="exemplo@hotmail.com"
-    required>
-    </v-text-field>
+            <v-text-field :rules="emailRules" class="my-3 text-white" hide-details="auto" label="Email" type="email" name="email" clearable
+                placeholder="example@hotmail.com" required>
+            </v-text-field>
+            <input type="hidden" name="_captcha" value="false">
+            <input type="hidden" name="_next" value="http://localhost:3000/contact">
+            <v-textarea :rules="textRules" class="my-3 text-white" label="Message" name="message" clearable required>
+            </v-textarea>
 
-    <v-textarea class="my-3" label="Message" name="message"></v-textarea>
+            <v-hover v-slot="{ isHovering, props }" open-delay="150">
+                <v-btn v-bind="props" :elevation="isHovering ? 8 : 2" :class="{ 'on-hover': isHovering }" size="small"
+                    rounded="pill" color="black" class="mt-4" width="167px" height="34px" outlined type="submit"
+                    @click="validate">
+                    <span class="text-white"><strong>{{ spanText }}</strong></span>
+                </v-btn>
+            </v-hover>
+            <v-snackbar v-model="snackbar" color="success">
+                <span class="text-white text-center"><strong>{{ text }}</strong></span>
+            </v-snackbar>
+        </v-responsive>
+    </v-form>
 
-    <v-hover v-slot="{ isHovering, props }" open-delay="150">
-        <v-btn v-bind="props" :elevation="isHovering ? 8 : 2" :class="{ 'on-hover': isHovering }" size="small"
-          rounded="pill" color="black" class="mt-4" width="167px" height="34px" outlined type="submit">
-          <span><strong class="text-white">{{ spanText }}</strong></span>
-          
-        </v-btn>
-      </v-hover>
 </template>
 
 
 <script>
+ 
+ export default {
+    data: () => ({
+      spanText: "send",
+      snackbar: false,
+      text: "the form was sent successfully !!!",
+      valid: true,
+      emailRules: [
+        value => !!value || 'Required.',
+        value => (value || '').length <= 50 || 'Max 50 characters',
+        value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        },
+      ],
+        textRules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+        
+    }),
 
-export default{
-    data(){
-        return{
-            spanText: "send"
-        }
-    }
-}
+    methods: {
+      async validate () {
+        const { valid } = await this.$refs.form.validate()
+
+        if (valid) this.snackbar = true
+      },
+      
+    },
+  }
+ 
+    
 </script>
 
 
 <style>
-
+v-application .transparent {
+    border-color: black !important;
+}
 </style>
 
