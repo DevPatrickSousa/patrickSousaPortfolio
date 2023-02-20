7451ccbc452ef672abc11ad516838a29815493db
DevPatrickSousa <patrickseven22@hotmail.com>
2023-02-20 02:20:51 -0300

    updating about view

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,12 @@
 <template >
     <v-container class="fill-height ">
       <v-responsive class="d-flex align-center text-center fill-height ">
-        <div class="d-flex mt-1 justify-center text-center text-white">
+        <div class="d-flex  mb-12 justify-center text-center text-white">
             <span>Contact</span>
         </div>
         <div class="d-flex justify-center text-center ">
-            <v-card class="mt-12 rounded-xl" width="1500" height="700" color="black" :style="'border: 1px solid white'">
-                <div class="mt-10">
+            <v-card class=" rounded-xl" width="1500" height="650" color="black" :style="'border: 1px solid white'">
+                <div class="mt-12">
                   <a href="https://wa.me/5511943646430" target="_blank"><img src="/src/assets/whatsapp.ico" alt="whatsApp" ALIGN=middle>
                     <span class="mx-7 mt-5 text-white">55 (11)94364-6430</span></a>
                     <img src="/src/assets/outlook.ico" alt="outlook" ALIGN=middle>
@@ -14,7 +14,7 @@
                 </div>
 
                 <div class="d-flex justify-center align-center text-center">
-                    <v-card class="d-flex mt-16 rounded-xl  justify-center align-center  " width="900" height="510"
+                    <v-card class="d-flex mt-16 rounded-xl  justify-center align-center  " width="900" height="470"
                         variant="outlined">
 
                         <formComponent />
