44bf1cbb5812152dd0a7f841fe72eaef19278258
devpatricksousa <patrickseven22@hotmail.com>
2024-11-23 15:26:18 -0300

    starting updating contact screen

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,13 @@
-<template >
-  <v-container class="fill-height ">
-    <v-responsive class="d-flex align-center text-center fill-height ">
+<template>
+  <v-container fluid class="pa-0 fill-height"
+    style="background: linear-gradient(to right, rgba(13, 71, 161, 1), rgba(255, 255, 255, 0.9));">
+    <v-row class="fill-height">
+      <v-col cols="12" class="d-flex justify-center align-center">
+        <span class="text-center text-h5 text-uppercase text-white"> Em processo...</span>
+      </v-col>
+    </v-row>
+  </v-container>
+  <!-- <v-responsive class="d-flex align-center text-center fill-height ">
       <div class="d-flex  mb-12 justify-center text-center text-white">
         <span>Contact</span>
       </div>
@@ -22,15 +29,14 @@
           </div>
         </v-card>
       </div>
-    </v-responsive>
-  </v-container>
+    </v-responsive> -->
 </template>
-  
+
 <script setup>
 import formComponent from './formComponent.vue';
 </script>
-  
-  
+
+
 <style>
 .v-application__wrap {
   background-color: #212121;
@@ -54,4 +60,3 @@ a {
   text-decoration: none;
 }
 </style>
-  
