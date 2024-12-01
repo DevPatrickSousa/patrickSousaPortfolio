5674fa79cf22cd75066bb78bddfc52fea2d361a0
devpatricksousa <patrickseven22@hotmail.com>
2024-12-01 03:28:30 -0300

    updating home screen layout and changing background image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,7 +16,7 @@ const animatedDescription = ref('')
 const showButton = ref(false)
 const sound = ref(false)
 const noSound = ref(false)
-const topValue = ref('45vh')
+const topValue = ref('50vh')
 const index = ref(0)
 
 const soundDialog = () => sound.value = true
@@ -44,7 +44,7 @@ watch(sound, async (newVal, oldVal) => {
   if (newVal != oldVal) {
     noSound.value = true;
     showButton.value = false;
-    topValue.value = '45vh';
+    topValue.value = '50vh';
   }
 })
 
@@ -61,12 +61,12 @@ watch(locale, (newLocale, oldLocale) => {
 <template>
   <v-container fluid class="pa-0" style="height: 100vh; width: 100vw;">
     <v-card style="height: 100%; width: 100%; border-radius: 0px;"
-      :image="width < 960 ? '' : (width < 1220 ? euLessResolution : eu)"
-      :style="width < 960 ? { background: '#0E47A1', background: 'radial-gradient(at center, #0E47A1, #1E1D1D)' } : {}">
-      <div class="d-flex justify-end align-center">
+      :image="width < 1200 ? '' : (width < 1220 ? euLessResolution : eu)"
+      :style="width < 1200 ? { background: '#0E47A1', background: 'radial-gradient(at center, #0E47A1, #1E1D1D)' } : {}">
+      <div style="position: absolute; top: 2vh; left: 100%; transform: translateX(-100%);">
         <v-menu>
           <template v-slot:activator="{ props }">
-            <v-btn variant="plain" class="mr-3" v-bind="props" style="top: 7vh; background: none !important;">
+            <v-btn variant="plain" class="mr-3" v-bind="props" style="background: none !important;">
               <img :src="locale == 'pt' ? pt : en" alt="social.description"
                 :style="width > 1040 ? { maxWidth: '30px' } : { maxWidth: '30px' }">
             </v-btn>
@@ -90,91 +90,89 @@ watch(locale, (newLocale, oldLocale) => {
         </v-menu>
       </div>
 
-      <div
-        style="position: relative; background: linear-gradient(to bottom, rgba(57, 0, 255, 0.4), rgba(0, 0, 0, 0.5))">
+      <v-card-title class="text-h1 text-center text-uppercase" :class="width < 500 ? 'text-h3 !important' : 'none'"
+        style="position: absolute; max-width: 50%; color: white; font-weight: 200;" :style="{
+          top: width < 400 ? '12vh' : (width < 500 ? '12vh' : (width < 1200 ? '10vh' : '20vh')),
+          left: width < 1200 ? '50%' : '5vw',
+          transform: width < 1200 ? 'translate(-50%, -50%)' : 'none',
+          minWidth: width < 1200 ? '100%' : '50%',
+        }">
+        Patrick
+      </v-card-title>
 
-        <v-card-title class="text-uppercase text-h2 pt-0"
-          style="position: absolute; top: 3vh; left: 50%; transform: translateX(-50%); width: fit-content; color: white; text-align: center; padding: 20px; font-weight: 200;"
-          :style="{
-            top: width < 400 ? '0vh' : (width < 500 ? '60%' : ''),
-          }">
-          Patrick
-        </v-card-title>
+      <v-card-subtitle class="text-body-2 text-center text-uppercase pt-0"
+        :class="width < 500 ? 'text-caption !important' : 'none'"
+        style="position: absolute; max-width: 50%; color: white; font-weight: 200;" :style="{
+          top: width < 400 ? '17vh' : (width < 500 ? '16vh' : (width < 1200 ? '16vh' : '35vh')),
+          left: width < 1200 ? '50%' : '5vw',
+          transform: width < 1200 ? 'translate(-50%, -50%)' : 'none',
+          minWidth: width < 1200 ? '100%' : '50%',
+        }">
+        {{ t('home.subtitle') }}
+      </v-card-subtitle>
 
-        <v-card-subtitle class="text-body-2 text-uppercase pt-0"
-          style="position: absolute; top: 11vh; left: 0; width: 100%; color: white; text-align: center; padding: 20px; font-weight: 200;"
-          :style="{
-            top: width < 400 ? '11vh' : (width < 960 ? '8.5vh' : '8.5vh'),
-          }">
-          {{ t('home.subtitle') }}
-        </v-card-subtitle>
-      </div>
-
-      <div class="rounded-circle border-md" v-if="width < 960"
+      <div class="rounded-circle border-md" v-if="width < 1200"
         style="position: absolute; top: 22vh; left: 50%; transform: translateX(-50%);" :style="{
-          top: width < 400 ? '21.5vh' : (width < 500 ? '18vh' : (width < 960 ? '18vh' : '')),
+          top: width < 400 ? '20vh' : (width < 500 ? '18.5vh' : (width < 1200 ? '20vh' : '')),
         }">
         <v-img :width="width < 400 ? 200 : 320" :height="width < 400 ? 200 : 320" aspect-ratio="16/9" cover
           class="rounded-circle" src="../assets/euCircle.png"></v-img>
       </div>
 
-      <v-card-text class="text-h5 text-uppercase" :class="width < 960 ? 'text-center' : ''"
-        style="position: absolute; top: 30vh; left: 5vw; max-width: 50%; color: white; padding: 20px; font-weight: 300; line-height: 1.5;"
+      <v-card-text class="text-h5 text-uppercase" :class="width < 1200 ? 'text-center' : ''"
+        style="position: absolute; max-width: 50%; color: white; padding: 20px; font-weight: 300; line-height: 1.5;"
         :style="{
-          top: width < 400 ? '60vh' : (width < 500 ? '60vh' : (width < 960 ? '60%' : '30vh')),
-          left: width < 960 ? '50%' : '5vw',
-          transform: width < 960 ? 'translate(-50%, -50%)' : 'none',
-          minWidth: width < 960 ? '100%' : '',
-          fontSize: width < 600 ? '14px !important' : (width < 960 ? '20px !important' : '')
+          top: width < 400 ? '60vh' : (width < 500 ? '60vh' : (width < 1200 ? '60%' : '40vh')),
+          left: width < 1200 ? '50%' : '5vw',
+          transform: width < 1200 ? 'translate(-50%, -50%)' : 'none',
+          minWidth: width < 1200 ? '100%' : '',
+          fontSize: width < 600 ? '14px !important' : (width < 1200 ? '14px !important' : '20px !important')
         }">
         {{ animatedDescription }}
       </v-card-text>
 
       <v-card-actions :style="{
-        top: width < 400 ? '67vh' : (width < 960 ? '65vh' : (width < 1920 ? '58vh' : topValue)),
-        left: width < 960 ? '50%' : '5vw',
-        width: width < 400 ? '67vh' : (width < 960 ? '65vh' : (width < 1920 ? '58vh' : topValue)),
-        width: width < 960 ? '85vw' : '50%',
+        top: width < 400 ? '67vh' : (width < 1200 ? '65vh' : (width < 1920 ? '53vh' : topValue)),
+        left: width < 1200 ? '50%' : '5vw',
+        width: width < 1200 ? '85vw' : '50%',
         padding: '20px',
         fontSize: '1vw',
         textTransform: 'uppercase',
         display: 'flex',
         justifyContent: 'center',
         position: 'absolute',
-        transform: width < 960 ? 'translateX(-50%)' : ''
+        transform: width < 1200 ? 'translateX(-50%)' : ''
       }">
-        <v-container :style="width < 960 ? { minWidth: '50vw' } : {}">
+        <v-container class="px-0" :style="width < 1200 ? { minWidth: '50vw' } : {}">
           <v-row>
-            <v-col cols="12" class="w-100">
+            <v-col cols="12" class="w-100 pl-0">
               <span v-if="showButton == true" class="d-flex justify-center text-white" :style="{
-                fontSize: width < 500 ? '2.5vw' : (width < 960 ? '2vw' : '16px'),
+                fontSize: width < 500 ? '2.5vw' : (width < 1200 ? '2vw' : '16px'),
                 fontWeight: 300
-
               }">
                 {{ t('home.music') }}
               </span>
-
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="6" :class="width < 960 ? 'py-0' : ''">
-              <v-btn v-if="showButton == true" color="#0d47a1" @click="soundDialog"
+            <v-col cols="6" :class="width < 990 ? 'py-0' : ''">
+              <v-btn v-if="showButton == true" rounded="lg" color="#0d47a1" @click="soundDialog"
                 style="background-color: white; min-height: 50px; font-size: 15px; width: 100%;"
-                :style="width < 960 ? { fontSize: '15px' } : {}">
+                :style="width < 990 ? { fontSize: '15px' } : {}">
                 {{ t('home.buttons.confirm') }}
               </v-btn>
             </v-col>
-            <v-col cols="6" :class="width < 960 ? 'py-0' : ''">
-              <v-btn v-if="showButton == true" color="#0d47a1" @click="noSoundDialog"
+            <v-col cols="6" :class="width < 990 ? 'py-0' : ''">
+              <v-btn v-if="showButton == true" rounded="lg" color="#0d47a1" @click="noSoundDialog"
                 style="background-color: white; min-height: 50px; font-size: 15px; width: 100%;"
-                :style="width < 960 ? { fontSize: '15px' } : {}">
+                :style="width < 990 ? { fontSize: '15px' } : {}">
                 {{ t('home.buttons.cancel') }}
               </v-btn>
             </v-col>
             <v-col cols="12" class="mx-auto" style="display: flex; justify-content: center;">
-              <v-btn v-if="noSound == true" color="#0d47a1" to="/aboutme"
+              <v-btn v-if="noSound == true" rounded="lg" color="#0d47a1" to="/aboutme"
                 style="background-color: white; min-height: 50px; font-size: 15px; width: 50%;"
-                :style="width < 960 ? { fontSize: '15px' } : {}">
+                :style="width < 990 ? { fontSize: '15px' } : {}">
                 {{ t('home.buttons.details') }}
               </v-btn>
             </v-col>
